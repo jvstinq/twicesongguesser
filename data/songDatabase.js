0ff609137c92cd42869fb4ae2e12e1cb1208c287
jvstinq <justsienes200@gmail.com>
2025-03-30 02:07:48 -0500

    Added some songs, going to add the whole discography tomorrow

--- data/songDatabase.js
+++ data/songDatabase.js
@@ -1,17 +1,85 @@
 const songDatabase = [
-    { 
-        title: "FANCY", 
-        audio: "../assets/audio/fancy.mp3",  // Updated path
-        albumImage: "../assets/images/fancy.jpg",  // Updated path
-        album: "FANCY YOU"
+    
+    //WITH YOU-th Album
+
+    {
+        title: "I GOT YOU",
+        audio: "../assets/audio/igotyou.mp3",
+        albumImage: "../assets/images/withyou",
+        album: "WITH YOU-th",
     },
-    { 
-        title: "TT", 
-        audio: "../assets/audio/tt.mp3",
-        albumImage: "../assets/images/tt.jpg",
-        album: "TWICEcoaster: LANE 1"
+    {
+        title: "ONE SPARK",
+        audio: "../assets/audio/onespark",
+        albumImage: "../assets/images/withyou",
+        album: "WITH YOU-th",
     },
-    // Add all other songs...
+    {
+        title: "RUSH",
+        audio: "../assets/audio/rush",
+        albumImage: "../assets/images/withyou",
+        album: "WITH YOU-th",
+    },
+    {
+        title: "NEW NEW",
+        audio: "../assets/audio/newnew",
+        albumImage: "../assets/images/withyou",
+        album: "WITH YOU-th",
+    },
+    {
+        title: "BLOOM",
+        audio: "../assets/audio/bloom",
+        albumImage: "../assets/images/withyou",
+        album: "WITH YOU-th",
+    },
+    {
+        title: "YOU GET ME",
+        audio: "../assets/audio/yougetme",
+        albumImage: "../assets/images/withyou",
+        album: "WITH YOU-th",
+    },
+
+    //Dance Again idk if it's in another album
+
+    {
+        title: "Dance Again",
+        audio: "../assets/audio/danceagain",
+        albumImage: "../assets/images/danceagain",
+        album: "Dance Again",
+
+    },
+
+    //READY TO BE Album
+
+    {
+        title: "SET ME FREE",
+        audio: "../assets/audio/setmefree",
+        albumImage: "../assets/images/readytobe",
+        album: "READY TO BE",
+    },
+    {
+        title: "MOONLIGHT SUNRISE",
+        audio: "../assets/audio/moonlightsunrise",
+        albumImage: "../assets/images/readytobe",
+        album: "READY TO BE",
+    },
+    {
+        title: "GOT THE THRILLS",
+        audio: "../assets/audio/gotthethrills",
+        albumImage: "../assets/images/readytobe",
+        album: "READY TO BE",
+    },
+    
+
 ];
 
 export default songDatabase;
+
+/*
+    {
+        title: "",
+        audio: "",
+        albumImage: "",
+        album: "",
+    },
+*/
