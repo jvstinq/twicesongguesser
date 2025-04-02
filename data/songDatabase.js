820f92c49ad21fc02ef03a636f0e8af7ec3fcbe4
jvstinq <justsienes200@gmail.com>
2025-04-02 09:58:59 -0500

    More songs

--- data/songDatabase.js
+++ data/songDatabase.js
@@ -204,6 +204,208 @@ const songDatabase = [
         album: "Celebrate",
     },
 
+    //formulaoflove
+    {
+        title: "SCIENTIST",
+        audio: "../assets/audio/scientist",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "MOONLIGHT",
+        audio: "../assets/audio/moonlight",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "ICON",
+        audio: "../assets/audio/icon",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "CRUEL",
+        audio: "../assets/audio/cruel",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "REAL YOU",
+        audio: "../assets/audio/realyou",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "F.I.L.A (Fall In Love Again)",
+        audio: "../assets/audio/fila",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "LAST WALTZ",
+        audio: "../assets/audio/lastwaltz",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "ESPRESSO",
+        audio: "../assets/audio/espresso",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "REWIND",
+        audio: "../assets/audio/rewind",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "CACTUS",
+        audio: "../assets/audio/cactus",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "PUSH & PULL(JIHYO, SANA, DAHYUN)",
+        audio: "../assets/audio/pushandpull",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    { //FIX THIS
+        title: "HELLO",
+        audio: "../assets/audio/hello",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "1,3,2",
+        audio: "../assets/audio/132",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "CANDY",
+        audio: "../assets/audio/candy",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "The Feels",
+        audio: "../assets/audio/thefeels",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+
+    //Perfect World album
+
+    {
+        title: "Perfect World",
+        audio: "../assets/audio/perfectworld",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "BETTER",
+        audio: "../assets/audio/better",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "Good at Love",
+        audio: "../assets/audio/goodatlove",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "Fanfare",
+        audio: "../assets/audio/fanfare",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "Kura Kura",
+        audio: "../assets/audio/kurakura",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "Four-leaf Clover",
+        audio: "../assets/audio/fourleafclover",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "In the summer",
+        audio: "../assets/audio/inthesummer",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "PIECES OF LOVE",
+        audio: "../assets/audio/piecesoflove",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "Thank you, Family",
+        audio: "../assets/audio/thankyoufamily",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "PROMISE",
+        audio: "../assets/audio/promimse",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+
+    //I love you more than anyone
+    {
+        title: "I love you more than anyone",
+        audio: "../assets/audio/iloveyoumorethananyone",
+        albumImage: "../assets/images/hospital",
+        album: "Hospital Playlist Season 2 Pt. 4",
+    },
+
+    //Taste of Love
+
+    {
+        title: "Alcohol-Free",
+        audio: "../assets/audio/alcoholfree",
+        albumImage: "../assets/images/tasteoflove",
+        album: "Taste of Love",
+    },
+
+    {
+        title: "First Time",
+        audio: "../assets/audio/firsttime",
+        albumImage: "../assets/images/tasteoflove",
+        album: "Taste of Love",
+    },
+    {
+        title: "Scandal",
+        audio: "../assets/audio/scandal",
+        albumImage: "../assets/images/tasteoflove",
+        album: "Taste of Love",
+    },
+    {
+        title: "Conversation",
+        audio: "../assets/audio/conversation",
+        albumImage: "../assets/images/tasteoflove",
+        album: "Taste of Love",
+    },
+    {
+        title: "Baby Blue Love",
+        audio: "../assets/audio/babybluelove",
+        albumImage: "../assets/images/tasteoflove",
+        album: "Taste of Love",
+    },
+    {
+        title: "SOS",
+        audio: "../assets/audio/sos",
+        albumImage: "../assets/images/tasteoflove",
+        album: "Taste of Love",
+    },
 ];
 
 export default songDatabase;
