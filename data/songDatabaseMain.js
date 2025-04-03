7a0586fa33939d854e563f49b6668fb70c9eed3f
jvstinq <justsienes200@gmail.com>
2025-04-03 11:28:50 -0500

    Added another database of songs that are just popular songs

---
+++ data/songDatabaseMain.js
@@ -0,0 +1,111 @@
+const songDatabase2 = [
+    
+    {
+        title: "I GOT YOU",
+        audio: "../assets/audio/igotyou",
+        albumImage: "../assets/images/withyou",
+        album: "WITH YOU-th",
+    },
+
+    {
+        title: "Hare Hare",
+        audio: "../assets/audio/harehare",
+        albumImage: "../assets/images/dive",
+        album: "DIVE",
+    },
+    {
+        title: "Strategy",
+        audio: "../assets/audio/strategy",
+        albumImage: "../assets/images/strategy",
+        album: "STRATEGY",
+    },
+    {
+        title: "SET ME FREE",
+        audio: "../assets/audio/setmefree",
+        albumImage: "../assets/images/readytobe",
+        album: "READY TO BE",
+    },
+    {
+        title: "MOONLIGHT SUNRISE",
+        audio: "../assets/audio/moonlightsunrise",
+        albumImage: "../assets/images/readytobe",
+        album: "READY TO BE",
+    },
+    {
+        title: "Talk that Talk",
+        audio: "../assets/audio/talkthattalk",
+        albumImage: "../assets/images/between1and2",
+        album: "BETWEEN 1&2",
+    },
+    {
+        title: "Celebrate",
+        audio: "../assets/audio/celebrate",
+        albumImage: "../assets/images/celebrate",
+        album: "Celebrate",
+    },
+    {
+        title: "Doughnut",
+        audio: "../assets/audio/doughnut",
+        albumImage: "../assets/images/celebrate",
+        album: "Celebrate",
+    },
+    {
+        title: "SCIENTIST",
+        audio: "../assets/audio/scientist",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "The Feels",
+        audio: "../assets/audio/thefeels",
+        albumImage: "../assets/images/formulaoflove",
+        album: "Formula of Love:O+T=<3",
+    },
+    {
+        title: "Perfect World",
+        audio: "../assets/audio/perfectworld",
+        albumImage: "../assets/images/perfectworld",
+        album: "Perfect World",
+    },
+    {
+        title: "Alcohol-Free",
+        audio: "../assets/audio/alcoholfree",
+        albumImage: "../assets/images/tasteoflove",
+        album: "Taste of Love",
+    },
+    {
+        title: "I CAN'T STOP ME",
+        audio: "../assets/audio/icantstopme",
+        albumImage: "../assets/images/eyeswideopen",
+        album: "Eyes Wide Open",
+    },
+    {
+        title: "MORE & MORE",
+        audio: "../assets/audio/moreandmore",
+        albumImage: "../assets/images/moreandmore",
+        album: "MORE & MORE",
+    },
+    {
+        title: "Feel Special",
+        audio: "../assets/audio/feelspecial",
+        albumImage: "../assets/images/feelspecial",
+        album: "Feel Special",
+    },
+    {
+        title: "LOVE FOOLISH",
+        audio: "../assets/audio/lovefoolish",
+        albumImage: "../assets/images/feelspecial",
+        album: "Feel Special",
+    },
+];
+
+export default songDatabase2;
+
+/*
+    {
+        title: "",
+        audio: "../assets/audio/",
+        albumImage: "../assets/images/",
+        album: "",
+    },
+*/
